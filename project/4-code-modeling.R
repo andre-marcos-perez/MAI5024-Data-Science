bdd075b2593f165ca4782812c66438f76cdaa105
Andre Perez <andre.marcos.perez@gmail.com>
2019-05-03 10:30:10 -0300

    Update 4-code-modeling.R

--- project/4-code-modeling.R
+++ project/4-code-modeling.R
@@ -15,10 +15,13 @@ cat("\014")
 
 # -- Environment
 
-PATH.INPUT  <- "dataset"
-FILE.INPUT  <- "kaggle-preprocessing.csv"
+PATH.INPUT <- "dataset"
+FILE.INPUT <- "kaggle-preprocessing.csv"
+GLOB.ENVIR <- "models.RDATA"
 
 # -- Script
+
+LOAD.DATA = TRUE;
 
 # ------------------------------------------------------------------------------
 # -- Library -------------------------------------------------------------------
@@ -43,32 +46,64 @@ data <- read.csv(file.path(PATH.INPUT, FILE.INPUT), na.strings=c("NA"), encoding
 # -- Modeling ----------------------------
 # ----------------------------------------
 
-data$Job <- as.factor(data$Job);
+if(LOAD.DATA == TRUE) {
+  
+  load(GLOB.ENVIR);
+  
+} else {
+  
+  data$Job <- as.factor(data$Job);
 
-# train control
+  # train control
+  
+  train.ctrl <- trainControl(
+    method = "cv",
+    number = 10,
+    verboseIter = TRUE
+  );
+  
+  # model
+  
+  print(" - Logistic Regression")
+  model.lr.rt <- Sys.time();
+  model.lr <- train(Job ~ ., data, method="logreg", trControl=train.ctrl);
+  model.lr.rt <- Sys.time() - model.lr.rt;
+  model.lr.cm <- confusionMatrix(predict(model.lr, data), data$Job);
+  
+  print(" - Random Florest")
+  model.rf.rt <- Sys.time();
+  model.rf <- train(Job ~ ., data, method="rf", trControl=train.ctrl);
+  model.rf.rt <- Sys.time() - model.rf.rt;
+  model.rf.cm <- confusionMatrix(predict(model.rf, data), data$Job);
+  
+  print(" - XGBoost")
+  model.xg.rt <- Sys.time();
+  model.xg <- train(Job ~ ., data, method="xgbDART", trControl=train.ctrl);
+  model.xg.rt <- Sys.time() - model.xg.rt;
+  model.xg.cm <- confusionMatrix(predict(model.xg, data), data$Job);
+  
+  save.image(GLOB.ENVIR);
+  
+}
 
-train.ctrl <- trainControl(
-  method = "cv",
-  number = 10,
-  verboseIter = TRUE
-);
+# wilcoxon signed-ranks test
 
-# model
+wilcox.test(model.lr.cm$byClass, model.rf.cm$byClass, paired = TRUE, conf.int = TRUE);
+wilcox.test(model.lr.cm$byClass, model.xg.cm$byClass, paired = TRUE, conf.int = TRUE);
+wilcox.test(model.rf.cm$byClass, model.xg.cm$byClass, paired = TRUE, conf.int = TRUE);
 
-print(" - Logistic Regression")
-model.lr <- train(Job ~ ., data, method="logreg", trControl=train.ctrl);
-model.lr.cm <- confusionMatrix(predict(model.lr, data), data$Job);
+# var importance
 
-print(" - Random Florest")
-model.rf <- train(Job~., data, method="rf", trControl=train.ctrl);
-model.rf.cm <- confusionMatrix(predict(model.rf, data), data$Job);
+model.rf.varimp <- varImp(model.rf)$importance;
+model.rf.varimp <- cbind(rownames(model.rf.varimp), model.rf.varimp);
+model.rf.varimp <- as.data.frame(model.rf.varimp[order((model.rf.varimp$Overall), decreasing = TRUE),]);
 
 # ------------------------------------------------------------------------------
 # -- Clean ---------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 
-rm(list = c("FILE.INPUT", "PATH.INPUT"));
-rm(list = c("train.ctrl"));
+rm(list = c("FILE.INPUT", "PATH.INPUT", "GLOB.ENVIR"));
+rm(list = c("train.ctrl","data"));
 
 # ------------------------------------------------------------------------------
 # -- Done ---------------------------------------------------------------------
